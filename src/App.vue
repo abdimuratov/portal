33366c12287b81017559357aa4f20cf7bb56f058
Babonya <abdimuratov8@gmail.com>
2023-07-16 23:54:55 +0600

    sd

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <nav>
+        <RouterLink to="/">Главная</RouterLink>
+        <RouterLink to="/manual">Руководство</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
